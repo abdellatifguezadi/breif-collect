a5f62a4dc228163672bf365a8a788c04df6722c9
abdellatifguezadi <abdellatifguezadi@gmail.com>
2024-12-31 11:03:24 +0100

    final

--- views/admin/users/delete.php
+++ views/admin/users/delete.php
@@ -2,21 +2,12 @@
 include __DIR__.'/../../layouts/header.php';
 include __DIR__.'/../../../database/connection.php';
 
-
-if (!isset($_SESSION['user_id']) || $_SESSION['role_id'] != 1) {
-    header("Location: ../../auth/login.php");
-    exit();
-}
-
-
 if (!isset($_GET['id'])) {
+   $user_id = (int)$_GET['id'];
+    $query = "DELETE FROM user WHERE id = $user_id";
     header("Location: ../dashboard.php");
     exit();
 }
-
-$user_id = (int)$_GET['id'];
-$query = "DELETE FROM user WHERE id = $user_id";
-
 
 header("Location: ../dashboard.php");
 exit(); 
